a07ffe0aab343dc0aface3ab28630a7aea1e6903
tockata <anatoly.angelov@gmail.com>
2014-11-10 23:03:06 +0200

    added 11-CheckTheBrackets/bracketsChecker.js solution

--- 03-JavaScript-Loops-Arrays-Strings-Homework/11-CheckTheBrackets/bracketsChecker.js
+++ 03-JavaScript-Loops-Arrays-Strings-Homework/11-CheckTheBrackets/bracketsChecker.js
@@ -1,3 +1,26 @@
 function checkBrackets(str) {
+    var expression = str;
+    var leftBracket = 0;
+    var rightBracket = 0;
 
+    for(var i = 0; i < expression.length; i++) {
+        if(expression[i] === '(') {
+            leftBracket++;
+        } else if (expression[i] === ')' && rightBracket < leftBracket) {
+            rightBracket++;
+        } else if (expression[i] === ')' && rightBracket >= leftBracket) {
+            console.log('incorrect');
+            return;
+        }
+    }
+
+    if(leftBracket === rightBracket) {
+        console.log('correct');
+    } else {
+        console.log('incorrect');
+    }
 }
+
+checkBrackets('( ( a + b ) / 5 – d )');
+checkBrackets(') ( a + b ) )');
+checkBrackets('( b * ( c + d *2 / ( 2 + ( 12 – c / ( a + 3 ) ) ) )');
---
+++ 03-JavaScript-Loops-Arrays-Strings-Homework/16-CardsFrequencies/cardFrequencies.js
@@ -0,0 +1,3 @@
+function findCardFrequency(str) {
+
+}
