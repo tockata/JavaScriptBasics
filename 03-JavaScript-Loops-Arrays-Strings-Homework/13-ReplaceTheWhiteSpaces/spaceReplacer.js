37a737d99c5eb5ab9bd9aae97738c862a42e7f86
tockata <anatoly.angelov@gmail.com>
2014-11-13 00:11:58 +0200

    minor changes

--- 03-JavaScript-Loops-Arrays-Strings-Homework/13-ReplaceTheWhiteSpaces/spaceReplacer.js
+++ 03-JavaScript-Loops-Arrays-Strings-Homework/13-ReplaceTheWhiteSpaces/spaceReplacer.js
@@ -1,9 +1,8 @@
 function replaceSpaces(str) {
     var text = str;
-    text = text.replace(/\s+/g, '&nbsp;');
+    text = text.replace(/\s+/g, '');
 
     console.log(text);
 }
-
 
 replaceSpaces('But you were living in another world tryin\' to get your message through');
